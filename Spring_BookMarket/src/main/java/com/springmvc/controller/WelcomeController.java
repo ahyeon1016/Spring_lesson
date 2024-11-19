84ce969921d4450891015607abcfb10d391fe6bf
GangHyeon An <cccloud1016@gmail.com>
2024-11-19 10:55:18 +0900

    2024.11.19 [Create] [Spring-Security / authorize]

--- Spring_BookMarket/src/main/java/com/springmvc/controller/WelcomeController.java
+++ Spring_BookMarket/src/main/java/com/springmvc/controller/WelcomeController.java
@@ -7,6 +7,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WelcomeController {
+
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String first(Model model) {
+		model.addAttribute("greeting", "Welcome to BookMarket");
+		model.addAttribute("strapline", "Welcom to Web Shopping Mall");
+		System.out.println(model.getAttribute("greeting"));
+		return "welcome";
+	}
 	
 	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public String welcome(Model model) {
--- chap08/src/main/java/com/springmvc/chap08/Exam08_03_Controller.java
+++ chap08/src/main/java/com/springmvc/chap08/Exam08_03_Controller.java
@@ -9,11 +9,13 @@ public class Exam08_03_Controller {
 
 	@GetMapping("/exam03")
 	public String requestMethod(Model model) {
+		System.out.println("컨트롤러 | /exam03 매핑");
 		return "webpage08_03";
 	}
 	
 	@GetMapping("/admin/tag")
 	public String requestMethod02(Model model) {
+		System.out.println("컨트롤러 | /admin/tag 매핑, 권한인증");
 		return "webpage08_03";
 	}
 }
