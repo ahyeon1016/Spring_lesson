b0f752627fef2f0aa3e94d7492b0f0885d6a7228
GangHyeon An <cccloud1016@gmail.com>
2024-11-25 11:27:43 +0900

    2024.11.25 [Create] [DTO ㅡ> JSON]

--- Spring_BookMarket/src/main/webapp/resources/js/controllers.js
+++ Spring_BookMarket/src/main/webapp/resources/js/controllers.js
@@ -4,14 +4,20 @@ function addToCart(action){
 	alert("도서가 장바구니에 추가되었습니다.");
 }
 
-function removeFromCart(action){
-	document.removeForm.action = action;
-	document.removeForm.submit();
-	window.location.reload();
-	alert("도서가 장바구니에서 삭제되었습니다.");
+function removeFromCart(action)
+{
+   document.removeForm.action = action;
+   document.removeForm.submit();
+   setTimeout(function()
+   {
+      window.location.reload();
+   }, 100)
 }
 
 function clearCart(){
 	document.clearForm.submit();
-	window.location.reload();
+	setTimeout(function()
+	{
+	   window.location.reload();
+	}, 100)
 }
